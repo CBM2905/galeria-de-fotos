3c4da9aa47d96fcef284e909498208274452e9e1
CBM2905 <cbmillos29@gmail.com>
2025-03-09 13:31:44 -0500

    app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, input } from '@angular/core';
+import { Firestore, collection, addDoc, CollectionReference, collectionData, updateDoc, doc, deleteDoc } from '@angular/fire/firestore';
+import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,7 +9,106 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  private firestore: Firestore = inject(Firestore);
+  test$!: Observable<Test[]>;
+  constructor(private alertController: AlertController) {
+    const userColl = collection(this.firestore, "test");
+    this.test$ = collectionData(userColl, {idField: "uid"}) as Observable<Test[]>;
+  }
 
-  constructor() {}
+  
+  crear(Nombre: string, Glucose: number, BMI: number){
+    addDoc(collection(this.firestore, "test"), {Nombre: Nombre, Glucose: Glucose, BMI: BMI});
+    console.log(this.test$);
+  }
 
+  update(Nombre: string, Glucose: number, BMI: number, uid: any){
+    let refDoc = doc(this.firestore, "test", uid);
+    updateDoc(refDoc, {Nombre: Nombre, Glucose: Glucose, BMI: BMI});
+  }
+
+  delete(uid: any){
+    let refDoc = doc(this.firestore, "test", uid);
+    deleteDoc(refDoc);
+  }
+
+
+
+  public alertButtons = [
+    {
+      text: "confirm",
+      handler: (data: any) => {
+        let nombre = data.Nombre;
+        let glucose = data.Glucosa
+        let bmi = data.BMI;
+
+        this.crear(nombre,glucose,bmi);        
+      }       
+    }
+  ]
+  public alertInputs = [
+    {
+      name: "Nombre",
+      placeholder: "ingrese el nombre",
+      type: "string"
+    },
+    {
+      name: "Glucosa",
+      placeholder: "Ingrese Glucosa",
+      type: "number"
+    },
+    {
+      name: "BMI",
+      placeholder: "indice de masa corporal",
+      type: "number"
+    }
+  ]
+
+  async showUpdateAlert(info: any){
+    let alert = await this.alertController.create({
+      header: "edit",
+      inputs: [
+        {
+          name: "Nombre",
+          placeholder: info.Nombre,
+          value: info.Nombre,
+          type: 'text'
+        },
+        {
+          name: "Glucosa",
+          placeholder: info.Glucose,
+          type: "number"
+        },
+        {
+          name: "BMI",
+          placeholder: info.BMI,
+           type: "number"
+        }
+      ],
+      buttons: [
+        {
+          text: "editar",
+          handler: data => {
+            let nombre = data.Nombre;
+            let glucose = data.Glucosa;
+            let bmi = data.BMI;
+
+            this.update(nombre,glucose,bmi,info.uid);
+          }
+        }
+      ]
+    })
+    await alert.present();
+    
+  }
+  
+}
+
+
+export interface Test {
+  Nombre: String,
+  Glucose: number,
+  BMI: number,
+
+  uid: String
 }
